5dedd9adcde3279e0d4cd956631fe6f729064b7c
AmandaSteffano <141163422+Amandaetequiana@users.noreply.github.com>
2024-03-05 20:42:55 -0300

    Add files via upload

---
+++ exercicio2_trapezio.kt
@@ -0,0 +1,9 @@
+package funcoes
+
+fun main() {
+    val lado1 = 4
+    val lado2 = 2
+    val altura = 6
+    val resultado = (altura/2 * (lado1 + lado2));
+    println("A área do trapézio é: $resultado")
+}
